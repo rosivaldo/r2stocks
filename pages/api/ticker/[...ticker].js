7288f3af172e674ba274ca870321378e97b94618
Rosivaldo Ramalho <rosivaldo@gmail.com>
2021-01-21 15:27:05 -0300

    cache control ttl error

--- pages/api/ticker/[...ticker].js
+++ pages/api/ticker/[...ticker].js
@@ -12,13 +12,17 @@ export default async function index(request, response) {
     const payload = {"ticker": ticker[0], "prop": ticker[1] };
 
     if (request.query.key == selfSecret) {
-        const tickerResponse = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${payload.ticker}&apikey=${apiSecret}`);
-        const tickerResponseJSON = await tickerResponse.json();
-
-        const output = tickerResponseJSON[payload.prop];
-
-        response.setHeader('Cache-Control', `s-maxage=${cacheTtl}, stale-while-revalidate`);
-        response.status(200).json(output);
+        if (payload.prop == 'cache') {
+            response.status(200).json({output:payload.prop});
+        } else {
+            const tickerResponse = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${payload.ticker}&apikey=${apiSecret}`);
+            const tickerResponseJSON = await tickerResponse.json();
+    
+            const output = tickerResponseJSON[payload.prop];
+    
+            response.setHeader('Cache-Control', `s-maxage=${cacheTtl}, stale-while-revalidate`);
+            response.status(200).json(output);    
+        }
     } else {
         response.status(401).json({
             error: "Unauthorized"

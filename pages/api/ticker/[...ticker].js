05774242356fa3c039adb3af0e35bc6b60082ed3
Rosivaldo Ramalho <rosivaldo@gmail.com>
2021-01-21 15:00:30 -0300

    cache control ttl

--- pages/api/ticker/[...ticker].js
+++ pages/api/ticker/[...ticker].js
@@ -2,6 +2,8 @@ export default async function index(request, response) {
     
     const apiSecret = process.env.API_SECRET;
     const selfSecret = process.env.SELF_SECRET;
+    // cache invalidation time in minutes
+    const cacheTtl = process.env.CACHE_TTL;
     
     const {
         query: { ticker },
@@ -15,7 +17,7 @@ export default async function index(request, response) {
 
         const output = tickerResponseJSON[payload.prop];
 
-        response.setHeader('Cache-Control', 's-maxage=30, stale-while-revalidate');
+        response.setHeader('Cache-Control', `s-maxage=${cacheTtl}, stale-while-revalidate`);
         response.status(200).json(output);
     } else {
         response.status(401).json({
